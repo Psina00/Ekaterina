be9bf6f25f8f51e76bf2a8407de87a50a6a43282
Roman <romires2000@gmail.com>
2020-06-17 00:43:48 +0300

    fix pass

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('localhost', 'server', 'serv', 'pig');
+$link = mysqli_connect('localhost', 'server', 'server', 'pig');
     if (!$link) {
           die("Connection failed: " . mysqli_connect_error());
     }
